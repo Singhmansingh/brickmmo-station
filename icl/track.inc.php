fea1ab28a8a6fd537d796645178679fdea23244c
Shavin <64917885+Singhmansingh@users.noreply.github.com>
2023-06-03 10:12:21 -0400

    updated client

--- icl/track.inc.php
+++ icl/track.inc.php
@@ -3,23 +3,7 @@
 function track(){
 
     global $songs;
-    global $announcer;
 
-    $rand = rand(0,100);
-
-    if(false and ( (!isset($_SESSION['lastplayed'])) or (($rand > 80) and ($_SESSION['lastplayed'] != 'announcer')))) {
-        if(!isset($announcer) or count($announcer) <= 0)
-        {
-            $announcer = scandir(__DIR__.'/../audio/announcer');
-            $announcer = array_slice($announcer, 2);
-            shuffle($announcer);
-        }
-
-        $audio = 'announcer/'.array_shift($announcer);
-        $_SESSION['lastplayed'] = 'announcer';
-    }
-
-    else {
         if(!isset($songs) or count($songs) <= 0)
         {
             $songs = scandir(__DIR__.'/../audio');
@@ -29,12 +13,9 @@ function track(){
 
         $audio = array_shift($songs);
         $_SESSION['lastplayed'] = 'track';
-    }
-
 
     ?>
 
-    <!--<?=$rand?> <?=$_SESSION['lastplayed']?>-->
     <source src="audio/<?=$audio?>" type="audio/mpeg">
 
     <?php
